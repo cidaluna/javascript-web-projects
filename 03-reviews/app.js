84398ca7991cb39d151854a4368b47ad5c3fa946
cidaluna <cidalunna@hotmail.com>
2022-06-04 16:17:47 -0300

    adjust js array and styles

--- 03-reviews/app.js
+++ 03-reviews/app.js
@@ -1,3 +1,50 @@
+const reviews = [
+    {
+      id: 1,
+      name: "Sara J.",
+      job: "Web Developer JS",
+      img:
+        "/sara.jpg",
+      info:
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+         exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    },
+    {
+      id: 2,
+      name: "Jhon W.",
+      job: "UX Designer",
+      img:
+      "/jhon.jpg",
+        info:
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+         exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    },
+    {
+      id: 3,
+      name: "Julia M.",
+      job: "Web Developer Senior",
+      img:
+        "/julia.jpg",
+      info:
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+       exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    },
+    {
+      id: 4,
+      name: "Ruth A.",
+      job: "IT Manager",
+      img:
+        "/ruth.jpg",
+      info:
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+       exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    }
+  ];
+
 const img = document.getElementById("person-img");
 const author = document.getElementById("author");
 const job = document.getElementById("job");
@@ -10,5 +57,10 @@ const randomBtn = document.querySelector(".random-btn");
 let currentItem = 0;
 
 window.addEventListener("DOMContentLoaded", function (){
-    console.log("teste");
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.info;
 });
+
