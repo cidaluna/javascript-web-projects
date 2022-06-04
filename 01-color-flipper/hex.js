8622b64f542f88de9d33dfa1c184314d1ed562ab
cidaluna <cidalunna@hotmail.com>
2022-06-03 23:03:26 -0300

    added generate your own hex color code

---
+++ 01-color-flipper/hex.js
@@ -0,0 +1,21 @@
+// What about generate your own hex color code  =) 
+const colors = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+
+btn.addEventListener("click", () => {
+    var hex = "#";
+    for(let i=0; i<6; i++){
+        hex += colors[randomNumber()];
+    }
+    console.log(hex);
+    document.body.style.backgroundColor = hex;
+    color.innerHTML = hex;
+});
+
+function randomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
+
