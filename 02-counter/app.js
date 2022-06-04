d506a0eeba3c6b38ccff75fb85d82f900728e0cc
cidaluna <cidalunna@hotmail.com>
2022-06-04 13:53:37 -0300

    implemented button behavior in vanilla javascript

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -1,10 +1,22 @@
 
-let num = 0;
+var num = 0;
 const zero = document.querySelector("#zero");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (item) {
     item.addEventListener("click", (event) => {
-        console.log(event.currentTarget);
-    })
+        //console.log(event.currentTarget.classList); //capture which was clicked and which class it is.
+        const styles = event.currentTarget.classList;
+        if(styles.contains("decrease")){
+            num--;
+        }else if(styles.contains("reset")){
+            num = 0;
+        }else{
+            num++;
+        }
+        zero.textContent = num;
+        if(num > 0){
+            zero.style.color = "green";
+        }
+    });
 });
