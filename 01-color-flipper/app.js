5cf6945f475f25273a4e29defe06c18583ca4e79
cidaluna <cidalunna@hotmail.com>
2022-06-03 21:33:44 -0300

    add main button change color with JS on index HTML

---
+++ 01-color-flipper/app.js
@@ -0,0 +1,9 @@
+const colors = ["green", "purple", "yellow", "brown"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    // get random number between 0 - 3
+    const randomNumber = 2;
+});
