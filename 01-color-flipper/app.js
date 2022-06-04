944d3948d46aae52a81233dd1812b066599fe955
cidaluna <cidalunna@hotmail.com>
2022-06-03 22:27:31 -0300

    add random number function and finished background color change

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,9 +1,18 @@
-const colors = ["green", "purple", "yellow", "brown"];
+//It is possible use the colors'name instead code hexadecimal
+//const colors = ["green", "red", "brown","purple", "white", "gray"];
+const colors = ["#f9aaaa", "#cccc55", "#d95542", "#b1b1b1",
+"#aa99f1", "#c6bc55", "#31d9d9", "#555444"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function(){
-    // get random number between 0 - 3
-    const randomNumber = 2;
+btn.addEventListener("click", () => {
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
