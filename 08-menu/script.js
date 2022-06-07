c1457cb305af1ce8f3eaeb2a2f66181d969fcaca
cidaluna <cidalunna@hotmail.com>
2022-06-07 19:11:18 -0300

    add menu itens project

---
+++ 08-menu/script.js
@@ -0,0 +1,94 @@
+// itens
+
+const menu = [
+    {
+        id:1,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `Lorem ipsum dolor, sed do eiusmod tempor it on their dry-transfer sheets.
+        incididunt ut labore et dolore magna aliqua. `,
+    },
+    {
+        id:2,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `Lorem ipsum dolor, sed do eiusmod tempor it on their dry-transfer sheets.
+        incididunt ut labore et dolore magna aliqua. `,
+    },
+    {
+        id:3,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `Lorem ipsum dolor, sed do eiusmod tempor it on their dry-transfer sheets.
+        incididunt ut labore et dolore magna aliqua. `,
+    },
+    {
+        id:4,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `Lorem ipsum dolor, sed do eiusmod tempor it on their dry-transfer sheets.
+        incididunt ut labore et dolore magna aliqua. `,
+    },
+    {
+        id:5,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `Lorem ipsum dolor, sed do eiusmod tempor it on their dry-transfer sheets.
+        incididunt ut labore et dolore magna aliqua. `,
+    },
+    {
+        id:6,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `Lorem ipsum dolor, sed do eiusmod tempor it on their dry-transfer sheets.
+        incididunt ut labore et dolore magna aliqua. `,
+    },
+    {
+        id:7,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `Lorem ipsum dolor, sed do eiusmod tempor it on their dry-transfer sheets.
+        incididunt ut labore et dolore magna aliqua. `,
+    },
+    {
+        id:8,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `Lorem ipsum dolor, sed do eiusmod tempor it on their dry-transfer sheets.
+        incididunt ut labore et dolore magna aliqua. `,
+    },
+    {
+        id:9,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `Lorem ipsum dolor, sed do eiusmod tempor it on their dry-transfer sheets.
+        incididunt ut labore et dolore magna aliqua. `,
+    },
+    {
+        id:10,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `Lorem ipsum dolor, sed do eiusmod tempor it on their dry-transfer sheets.
+        incididunt ut labore et dolore magna aliqua. `,
+    }
+]
