9f8b4608ad7bae2baafdbb3651d3dfeab97963e2
cidaluna <cidalunna@hotmail.com>
2022-06-06 21:27:36 -0300

    implement add and remove class on each cards

--- 07-questions/script.js
+++ 07-questions/script.js
@@ -1,12 +1,33 @@
 
-const btns = document.querySelectorAll(".question-btn");
-const text = document.querySelector(".question-text");
+// Using selectors inside the element
+const questions = document.querySelectorAll(".question");
 
+questions.forEach(function (question){
+    const btn = question.querySelector(".question-btn");
+    //console.log(btn);
+    btn.addEventListener("click", function () {
+        // console.log(question);
+    
+        questions.forEach(function (item) {
+          if (item !== question) {
+            item.classList.remove("show-text");
+          }
+        });
+    
+        question.classList.toggle("show-text");
+    });
+});
+
+
+/*
+* traversing the DOM
+const btns = document.querySelectorAll(".question-btn");
 btns.forEach(function(btn){
     btn.addEventListener("click", function(e){
         const question = e.currentTarget.parentElement.parentElement;
         question.classList.toggle("show-text");
     });
 });
+*/
 
 
