1f18eff2a45d220d642723c418c491f53d3d61fc
cidaluna <cidalunna@hotmail.com>
2022-06-17 20:33:38 -0300

    add configuration blocks in CSS

---
+++ 12-countdown-timer/script.js
@@ -0,0 +1,25 @@
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  
