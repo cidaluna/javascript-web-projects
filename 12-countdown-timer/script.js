53c747e5aeafab8810c9a3df4c8b071d74023b38
cidaluna <cidalunna@hotmail.com>
2022-06-17 22:22:20 -0300

    add values on each box like days hours

--- 12-countdown-timer/script.js
+++ 12-countdown-timer/script.js
@@ -27,7 +27,7 @@ const months = [
   const items = document.querySelectorAll('.deadline-format h4');
   // console.log(items);
 
-  let futureDate = new Date(2022,3,18,22,30,00); // month start in zero
+  let futureDate = new Date(2022,7,29,22,30,00); // month start in zero
  console.log(futureDate);
 
 const year = futureDate.getFullYear();
@@ -43,3 +43,45 @@ const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 
 giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${mins}pm`;
+
+// future time in miliseconds
+const futureTime = futureDate.getTime();
+console.log(futureTime);
+
+function getRemainingTime(){
+    const today = new Date().getTime();
+    const t = futureTime - today;
+    console.log(t);
+    // 1s = 1000ms
+    // 1m = 60s
+    // 1hr = 60min
+    // 1d = 24hr
+
+    // calculate 1 day values in ms
+    const oneDay = 24*60*60*1000;
+    console.log(oneDay);
+    const oneHour = 60*60*1000;
+    console.log(oneHour);
+    const oneMinute = 60*1000;
+    console.log(oneMinute);
+
+    // calculate all values
+    let days = t/oneDay;
+    console.log(days);
+    days = Math.floor(days);
+    
+    let hours = Math.floor((t % oneDay) / oneHour);
+    console.log(hours);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
+
+    // set values array
+    const values = [days, hours, minutes, seconds];
+
+    items.forEach(function(item, index){
+        item.innerHTML = values[index];
+    });
+
+}
+
+getRemainingTime();
