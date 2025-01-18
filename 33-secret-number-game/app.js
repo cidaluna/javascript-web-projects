9d07c943f195041b343f21b758e0447dd048e4ea
Cida Luna <cidalunna@hotmail.com>
2025-01-18 10:06:26 -0300

    Add attempt number to control game

--- 33-secret-number-game/app.js
+++ 33-secret-number-game/app.js
@@ -1,15 +1,26 @@
 alert('Boas vindas ao Jogo do Número Secreto');
 let secretNumber = 29;
-
-let userNumber = parseInt(prompt('Escolha um número entre 1 e 30'));
+let attempt = 1; // número de tentativas
+let userNumber;
 
 console.log(secretNumber);
 console.log(userNumber);
 console.log(typeof secretNumber);
 console.log(typeof userNumber);
 
-if (secretNumber === userNumber) {
-  alert('Parabéns! Você acertou o número secreto!!!');
-} else {
-  alert('Ops! Não foi dessa vez, tente novamente!')
+// enquanto o número do usuário nao for igual ao número secreto, exiba a pergunta
+while (userNumber != secretNumber){
+  userNumber = parseInt(prompt('Escolha um número entre 1 e 30? '));
+
+  if (secretNumber === userNumber) {
+    alert(`Parabéns! Você acertou o número secreto, com ${attempt} tentativa(s).`);
+  } else {
+    if (secretNumber > userNumber) {
+      alert(`O número secreto é maior do que o que você digitou (${userNumber}). Tente novamente!`);
+    } else {
+      alert(`O número secreto é menor do que o que você digitou (${userNumber}). Tente novamente!`);
+    }
+    attempt++;
+  }
+
 }
